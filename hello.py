5bbd2cd50c1c4216438825830d443dae30712553
chandu <141409100+Chandrakant6@users.noreply.github.com>
2024-10-16 00:37:50 +0530

    Update hello.py
    correct indentation

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 import time
 
 def hello(s = 'hello world'):
-  for i in range(len(s)):
-	  specialChar = '''!@#$%^&*(),./<>?;':"[]{}\|-_=+'''
-      l = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ' + specialChar
-      for j in l:
-          print(s[:i] + j)
-          time.sleep(.1) # seconds
-          if s[i] == j:
-              time.sleep(.5)
-              break
+for i in range(len(s)):
+	specialChar = '''!@#$%^&*(),./<>?;':"[]{}\|-_=+'''
+    l = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ' + specialChar
+    for j in l:
+		print(s[:i] + j)
+		time.sleep(.1) # seconds
+		if s[i] == j:
+			time.sleep(.5)
+			break
